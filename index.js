ba4746945af901d21ca1cc5993e51c642aa0702d
Allen Pottinger <allenpottinger@gmail.com>
2021-12-01 12:58:59 -0500

    deletes connection uri

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ import dotenv from 'dotenv';
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+
 
 app.use(cors());
 app.listen(PORT);
 
-const CONNECTION_URL = 'mongodb+srv://Allen7991:<password>@nostalgia.3qho6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
