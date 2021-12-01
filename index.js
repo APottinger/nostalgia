eff624e90e7ef597745100e19b1f7cc71e9b038e
Allen Pottinger <allenpottinger@gmail.com>
2021-12-01 13:27:00 -0500

    configures mongoose connection to mongodb

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import nodemon from 'nodemon';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
-
 app.use(cors());
-app.listen(PORT);
+app.listen(process.env.PORT, ()=> {console.log('listening')});
 
-const CONNECTION_URL = 'mongodb+srv://Allen7991:<password>@nostalgia.3qho6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(res => console.log('connected to db'))
+    .catch(error => console.log(error.messages))
+
+    
