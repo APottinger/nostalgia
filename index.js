baa1065c005300a421995d29043456857d55a0c0
Allen Pottinger <allenpottinger@gmail.com>
2021-12-01 12:58:08 -0500

    adds dotenv

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ import express from 'express';
 import nodemon from 'nodemon';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+app.use(cors());
+app.listen(PORT);
+
+const CONNECTION_URL = 'mongodb+srv://Allen7991:<password>@nostalgia.3qho6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
