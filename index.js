0905a34e585d65af556b8800cca06b0b396fd41e
Allen Pottinger <allenpottinger@gmail.com>
2021-12-02 11:50:20 -0500

    adds routes

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import postRoutes from './routes/post.js';
+
 const app = express();
 
 app.use(cors());
-app.listen(process.env.PORT, ()=> {console.log('listening')});
+app.listen(process.env.PORT, () => {console.log('listening')});
+
+app.use('/posts', postRoutes);
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(res => console.log('connected to db'))
