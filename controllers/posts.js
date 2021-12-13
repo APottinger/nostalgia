1524ed90c7c3da95fe6439379eba98127ae29c77
Allen Pottinger <allenpottinger@gmail.com>
2021-12-13 17:09:13 -0500

    creates post create action in controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,8 +7,22 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => {
     try {
-        await res.send('HELLO');
+        const postMessages = await Post.find();
+        console.log(postMessages)
+        res.status(200).json(postMessages);
     } catch (error) {
-        console.log(error);
+        res.status(404).json({message: error.message})
     }
+}
+
+export const createPost = async (req, res) => {
+        const post = req.body;
+        const newPost = new Post(post);
+
+        try {
+            await newPost.save();
+            res.status(201).json(newPost);
+        } catch (error) {
+            res.status(409).json({ message: error.message })
+        }
 }
